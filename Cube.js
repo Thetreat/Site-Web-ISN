8404eda8948ebd6436360c57523b611013d742c9
Maxime <zamax27@gmail.com>
2018-12-27 18:44:52 +0100

    Debut de about.html

---
+++ Cube.js
@@ -0,0 +1,24 @@
+var pointA, pointB;
+var x1 = -150,
+  y1 = -150,
+  z1 = 10,
+  x2 = 200,
+  y2 = 200,
+  z2 = 10;
+
+function setup() {
+  var canvas = createCanvas(601, 601, WEBGL);
+  canvas.parent('sketch-holder');
+  background(255, 0, 100);
+}
+
+function draw() {
+  background(200);
+  mouseDragged();
+  box(300, 300, 300, 9);
+}
+
+function mouseDragged() {
+  rotateX(radians(-mouseY) * 0.5);
+  rotateY(radians(-mouseX) * 0.5);
+}
--- tabs.js
+++ tabs.js
@@ -1,8 +1,7 @@
 function openChapter(nbChap) {
-    var i;
-    var x = document.getElementsByClassName("Chapitre");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    document.getElementById(nbChap).style.display = "block"; 
+  var x = document.getElementsByClassName("Chapitre");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  document.getElementById(nbChap).style.display = "block";
+}
